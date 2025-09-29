bc01e494d33c85b930de3c910c35aab30b7196c9
Vlad <vlad.petrovskiy00@mail.ru>
2025-09-29 17:05:10 +0300

    Реализация DSL

--- src/Apartament.kt
+++ src/Apartament.kt
@@ -1,5 +1,15 @@
 package app
 data class Apartament(var Rent: Double, var adress: Adress, var Area: Double, var CntRooms: Int,
-    var IsRented: Boolean) {}
+    var IsRented: Boolean) {
+
+    operator fun plus(other: Int): Apartament {
+        return Apartament(this.Rent + other, this.adress, this.Area, this.CntRooms, this.IsRented)
+    }
+
+    
+    //    override fun toString(): String {
+//        //return super.toString()
+//    }
+}
 
 data class Adress(var Street: String, var ApartmentNumb: Int, var HouseNumb: Int){}
--- src/ApartamentList.kt
+++ src/ApartamentList.kt
@@ -8,6 +8,11 @@ enum class SortCriteria {
     STRAIGHT, REVERSE
 }
 
+fun apartments(block: ApartamentList.() -> Unit): Unit {
+    return ApartamentList().block()
+}
+
+
 class ApartamentList() {
 
     private val innerDataBase: MutableList<Apartament> = mutableListOf()
@@ -29,33 +34,43 @@ class ApartamentList() {
         return false
     }
 
-    fun <T>filter(crit: T, typeCrit: PropertyCriteria): List<Apartament>?{
+    object by {
+        operator fun invoke(typeAndProperty: Pair<String, PropertyCriteria>) = typeAndProperty
+        operator fun invoke(sortAndPropCriterias: Pair<SortCriteria, PropertyCriteria>) =
+            sortAndPropCriterias
+    }
+
+    infix fun <T>filter(typeAndProperty: Pair <String, PropertyCriteria>): List<Apartament>?{
+        var(crit, typeCrit) = typeAndProperty
+
         when(typeCrit){
             PropertyCriteria.RENT ->
                 return innerDataBase.filter{
-                    it.Rent.toString().startsWith(crit.toString())
+                    it.Rent.toString().startsWith(crit)
                 }
             PropertyCriteria.NUMB->
                  return innerDataBase.filter{
-                    it.adress.ApartmentNumb.toString().startsWith(crit.toString())
+                    it.adress.ApartmentNumb.toString().startsWith(crit)
                 }
             PropertyCriteria.AREA ->
                 return innerDataBase.filter {
-                    it.Area.toString().startsWith(crit.toString())
+                    it.Area.toString().startsWith(crit)
                 }
             PropertyCriteria.CNTROOMS->
                 return  innerDataBase.filter {
-                    it.CntRooms.toString().startsWith(crit.toString())
+                    it.CntRooms.toString().startsWith(crit)
                 }
             PropertyCriteria.ISRENTED ->
                 return innerDataBase.filter {
-                    it.IsRented.toString().startsWith(crit.toString())
+                    it.IsRented.toString().startsWith(crit)
                 }
             else -> return null
         }
     }
-    
-    fun sort(sortCrit: SortCriteria, propCrit: PropertyCriteria): Unit {
+
+    //sortCrit: SortCriteria, propCrit: PropertyCriteria
+    infix fun sort(sortAndPropCriterias: Pair<SortCriteria, PropertyCriteria>): Unit {
+        var(sortCrit, propCrit) = sortAndPropCriterias
         when (sortCrit) {
             SortCriteria.STRAIGHT ->
                 when (propCrit) {
@@ -78,8 +93,11 @@ class ApartamentList() {
         return
     }
 
-    fun search(numb: Int): Apartament? {
+    infix fun search(numb: Int): Apartament? {
         return innerDataBase.find{it.adress.ApartmentNumb == numb}
     }
 
+
+
 }
+
